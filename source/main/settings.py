4053a6c7d833471e74203b25cb4ac3b70cfcf7b4
ernest_aitiev <valeetmihifaveas@gmail.com>
2019-12-13 15:14:44 +0600

    Шаблон контрольной работы: добавлены ссылки на вход\выход пользователей, отлажено получение токенов файл js обновлен

--- source/main/settings.py
+++ source/main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'webapp',
     'api_v1',
---
+++ source/webapp/static/js/api.js
@@ -0,0 +1,128 @@
+const baseUrl = 'http://localhost:8000/api/v1/';
+
+
+
+function getFullPath(path) {
+    path = path.replace(/^\/+|\/+$/g, '');
+    path = path.replace(/\/{2,}/g, '/');
+    return baseUrl + path + '/';
+}
+
+function makeRequest(path, method, auth=true, data=null) {
+    let settings = {
+        url: getFullPath(path),
+        method: method,
+        dataType: 'json'
+    };
+    if (data) {
+        settings['data'] = JSON.stringify(data);
+        settings['contentType'] = 'application/json';
+    }
+    if (auth) {
+        settings.headers = {'Authorization': 'Token ' + getToken()};
+    }
+    return $.ajax(settings);
+}
+
+function saveToken(token) {
+    localStorage.setItem('authToken', token);
+}
+
+function getToken() {
+    return localStorage.getItem('authToken');
+}
+
+function removeToken() {
+    localStorage.removeItem('authToken');
+}
+
+function logIn(username, password) {
+    const credentials = {username, password};
+    let request = makeRequest('login', 'post', false, credentials);
+    request.done(function(data, status, response) {
+        console.log('Received token');
+        saveToken(data.token);
+    }).fail(function(response, status, message) {
+        console.log('Could not get token');
+        console.log(response);
+    });
+}
+
+function logOut() {
+    let request = makeRequest('logout', 'post', true);
+    request.done(function(data, status, response) {
+        console.log('Cleaned token');
+        removeToken();
+        enterLink.removeClass('d-none');
+        exitLink.addClass('d-none');
+    }).fail(function(response, status, message) {
+        console.log('Could not clean token');
+        console.log(response.responseText);
+    });
+}
+let logInForm, homeLink, enterLink, exitLink, formSubmit, formTitle, content, formModal,
+    usernameInput, passwordInput
+
+function setUpGlobalVars() {
+    logInForm = $('#log_in_form');
+    homeLink = $('#home_link');
+    enterLink = $('#enter_link');
+    exitLink = $('#exit_link');
+    formSubmit = $('#form_submit');
+    formTitle = $('#form_title');
+    content = $('#content');
+    formModal = $('#form_modal');
+    usernameInput = $('#username_input');
+    passwordInput = $('#password_input');
+}
+
+
+function setUpAuth() {
+    logInForm.on('submit', function(event) {
+        event.preventDefault();
+        logIn(usernameInput.val(), passwordInput.val());
+    });
+
+    enterLink.on('click', function(event) {
+        event.preventDefault();
+        logInForm.removeClass('d-none');
+        // quoteForm.addClass('d-none');
+        formTitle.text('Войти');
+        formSubmit.text('Войти');
+        formSubmit.off('click');
+        formSubmit.on('click', function(event) {
+            logInForm.submit();
+        });
+    });
+
+    exitLink.on('click', function(event) {
+        event.preventDefault();
+        logOut();
+    });
+}
+
+function checkAuth() {
+    let token = getToken();
+    if(token) {
+        enterLink.addClass('d-none');
+        exitLink.removeClass('d-none');
+    } else {
+        enterLink.removeClass('d-none');
+        exitLink.addClass('d-none');
+    }
+}
+
+
+$(document).ready(function() {
+    setUpGlobalVars();
+    setUpAuth();
+    checkAuth();
+});
+
+
+//
+// $(document).ready(function() {
+//     let token = getToken();
+//     if (!token) logIn('ernest_aitiev', '2091');
+// });
+
